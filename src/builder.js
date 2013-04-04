a98e7b99b4ea0d5968e3866fa3a1ed601d3740f6
Louis-Philippe Huberdeau <lp@huberdeau.info>
2013-04-04 10:34:11 -0400

    Adding image copy in the build process

--- src/builder.js
+++ src/builder.js
@@ -6,35 +6,58 @@ var fs = require('fs')
 	, queue
 	;
 
-if (! fs.existsSync(target)) {
-	fs.mkdirSync(target);
+function handleDirectory(source, target) {
+	// Attempt folder creation, keep going even on failure / already exists
+	fs.mkdir(target, function () {
+		fs.readdir(source, function (err, files) {
+			files = files.map(function (fileName) {
+				console.log(source + fileName);
+				return {
+					fileName: fileName,
+					source: source,
+					target: target
+				};
+			});
+			async.each(files, queue.push);
+		});
+	});
 }
 
-queue = async.queue(function (fileName, callback) {
-	var path = source + fileName;
+queue = async.queue(function (info, callback) {
+	var path = info.source + info.fileName
+		, target = info.target + info.fileName;
 
 	fs.stat(path, function (err, stats) {
 		if (stats.isDirectory()) {
+			handleDirectory(path + '/', target + '/');
 			callback();
 			return;
 		}
 
-		fs.readFile(path, {
-			encoding: "utf-8"
-		}, function (err, data) {
-			var replaced = data
-				.replace(/([^\s]+)\/\*\{(\w+)\}\*\//g, function (whole, value, key) {
-					key = "ui_" + key;
-					variables[key] = value;
-					return "@" + key;
-				})
-				.replace(/\.css/g, '.less')
-				;
+		if (info.fileName.substr(-4) === '.css') {
+			fs.readFile(path, {
+				encoding: "utf-8"
+			}, function (err, data) {
+				var replaced = data
+					.replace(/([^\s]+)\/\*\{(\w+)\}\*\//g, function (whole, value, key) {
+						key = "ui_" + key;
+						variables[key] = value;
+						return "@" + key;
+					})
+					.replace(/\.css/g, '.less')
+					;
 
-			fs.writeFile(target + fileName.replace('.css', '.less'), replaced, function (err) {
-				callback();
+				fs.writeFile(target.replace('.css', '.less'), replaced, function (err) {
+					callback();
+				});
 			});
-		});
+		} else {
+			fs.readFile(path, function (err, data) {
+				fs.writeFile(target, data, function () {
+					callback();
+				});
+			});
+		}
 	});
 }, 5);
 
@@ -49,7 +72,5 @@ queue.drain = function () {
 	fs.writeFile(target + 'jquery.ui.less', "// Content auto-generated\n\n@import \"jquery.ui.variables.less\";\n@import \"jquery.ui.all.less\";\n");
 };
 
-fs.readdir(source, function (err, files) {
-	async.each(files, queue.push);
-});
+handleDirectory(source, target);
 
